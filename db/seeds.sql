4d1f1c4ba06b218c53c5cdf3b9286cca504629e5
Han Moon <hanmoon@Hans-MacBook-Pro.local>
2019-04-12 19:23:34 -0600

    changes

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO burgers (burger_name) 
+    VALUES ('Whopper');
+INSERT INTO burgers (burger_name) 
+    VALUES ('Baconator');
+INSERT INTO burgers (burger_name, devoured) 
+    VALUES ('Guac Burger', true);
+INSERT INTO burgers (burger_name, devoured) 
+    VALUES ('Spicy Chicken', true);
+INSERT INTO burgers (burger_name, devoured) 
+    VALUES ('Big Mac', true);
+INSERT INTO burgers (burger_name) 
+    VALUES ('Lettuce Burger');
